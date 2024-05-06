81a0c63facfd76753ac7fdecb2588f51d196039e
zawadinjagi <zawadinjagi619@gmail.com>
2024-05-06 20:47:04 +0300

    add

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,13 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div>About</div>;
+  return (
+    <div id="about">
+      <h2>About Me</h2>
+      <p>I am a hacker and software developer</p>
+      <img src={image} alt="I made this" />
+    </div>
+  );
 }
 
 export default About;
